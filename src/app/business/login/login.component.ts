ed509fe2d7e1c5b47bff953dc0939edf58f015a6
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-02-20 13:31:09 -0500

    C 20/02/2025 Realice las siguientes tareas: * Modifique el login completamente, con Logo de nosotros * Agregue en el header una funcion * Cambie la mayoria del texto a español

--- src/app/business/login/login.component.ts
+++ src/app/business/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import DashboardComponent from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [DashboardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
